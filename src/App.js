6636d07ce710f801ce57e19106f53e3320759e2c
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-07-14 14:29:01 -0700

    add components to render

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
+import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import { useParams, useNavigate } from 'react-router';
+import Search from './components/Search';
+import Results from './components/Results';
 
 function App() {
+	const [resource, setResource] = useState('');
+	const [resourceId, setResourceId] = useState(0);
+	const navigate = useNavigate();
+	
+
 	return (
 		<div>
 			<Routes>
-				<Route path='/' element={<Search />} />
 				<Route
-					path='/:collection/:id'
+					path='/'
+					element={
+						<Search
+						resource={resource}
+
+							navigate={navigate}
+							resourceId={resourceId}
+							setResourceId={setResourceId}
+							setResource={setResource}
+						/>
+					}
+				/>
+				<Route
+					path='/:resource/:id'
 					element={
 						<>
-							<Search />
-							<Collection />
+							<Search
+								resource={resource}
+
+								navigate={navigate}
+								resourceId={resourceId}
+								setResourceId={setResourceId}
+								setResource={setResource}
+							/>
+							<Results
+								resource={resource}
+								resourceId={resourceId}
+							/>
 						</>
 					}
 				/>
